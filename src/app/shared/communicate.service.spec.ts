7ce591d6a73c7fc44e7a52d934febf559b24dadb
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-08-28 16:27:05 +0200

    add test to communicate service

--- src/app/shared/communicate.service.spec.ts
+++ src/app/shared/communicate.service.spec.ts
@@ -1,15 +1,33 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { CommunicateService } from './communicate.service';
 
 describe('CommunicateService', () => {
+  let service: CommunicateService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [CommunicateService]
     });
+    service = TestBed.get(CommunicateService);
   });
 
-  it('should be created', inject([CommunicateService], (service: CommunicateService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('should change state when called show', async () => {
+    const msg = 'msg';
+    service.show(msg);
+
+    service.state$.subscribe(stream => {
+      expect(stream.message).toEqual(msg);
+    });
+  });
+
+  it('should change state when called hide', async () => {
+    service.hide();
+    service.state$.subscribe(stream => {
+      expect(stream.state).toBeFalsy();
+    });
+  });
 });
