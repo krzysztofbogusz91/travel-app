dd2eafd71c793ca1280354202f67b6089a9c6969
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-09-26 16:12:28 +0200

    creates loader component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommunicateComponent } from 'src/app/shared/communicate/communicate.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CommunicateComponent],
-  exports: [CommunicateComponent]
+  declarations: [CommunicateComponent, LoaderComponent],
+  exports: [CommunicateComponent, LoaderComponent]
 })
 export class SharedModule {}
