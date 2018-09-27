7ad01c4626ff84daafb3698d25286ebed4368111
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-09-27 15:57:28 +0200

    add form component and css

---
+++ src/app/trip-details/trip-form/trip-form.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TripFormComponent } from './trip-form.component';
+
+describe('TripFormComponent', () => {
+  let component: TripFormComponent;
+  let fixture: ComponentFixture<TripFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TripFormComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TripFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
