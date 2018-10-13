f6d41d8dcf19a42b08c4b8d57f146c4d43c45350
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-10-13 11:51:26 +0200

    add getCurretnTrip test

--- src/app/trip-details/current-trip-provider.service.spec.ts
+++ src/app/trip-details/current-trip-provider.service.spec.ts
@@ -1,17 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CurrentTripProviderService } from './current-trip-provider.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TripService } from 'src/app/shared/trip.service';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('CurrentTripProviderService', () => {
   let service: CurrentTripProviderService;
+  let tripService: TripService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [CurrentTripProviderService, TripService]
     });
     service = TestBed.get(CurrentTripProviderService);
+    tripService = TestBed.get(TripService);
   });
 
   it('should be created', () => {
@@ -22,5 +25,16 @@ describe('CurrentTripProviderService', () => {
     service.setId('2');
 
     expect(service.currentTripId).toEqual('2');
+  });
+
+  it('should get current trip', done => {
+    service.setId('2');
+
+    spyOn(tripService, 'getTrip').and.returnValue(of(true));
+
+    service.getCurrentTrip().subscribe(data => {
+      expect(tripService.getTrip).toHaveBeenCalledWith(service.currentTripId);
+      done();
+    });
   });
 });
